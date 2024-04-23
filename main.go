d650741905b309d0bb9b18c332fb33e4f09ca355
kaleab <808kaleabkzin@gmail.com>
2024-04-23 19:05:07 +0000

    simple file server

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
 func main() {
+	dir := http.Dir(".")
+
+	fileServer := http.FileServer(dir)
+
+	hideHiddenFiles := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/.") {
+			http.NotFound(w, r)
+			return
+		}
+		fileServer.ServeHTTP(w, r)
+	})
 	
+	http.Handle("/", hideHiddenFiles)
+
+	log.Println("starting on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
